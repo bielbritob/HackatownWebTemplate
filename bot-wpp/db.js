687449ac7613f959cc13528367bba0182e3d37de
bielbritob <123404262+bielbritob@users.noreply.github.com>
2026-05-18 22:27:35 -0400

    Update db.js

--- bot-wpp/db.js
+++ bot-wpp/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',        // usuário do MySQL local
-  password: 'k2g9ekk6',        // senha
+  password: '',        // senha
   database: '3a_engenharia_db', // mesmo banco que o PHP usa
   waitForConnections: true,
   connectionLimit: 10
